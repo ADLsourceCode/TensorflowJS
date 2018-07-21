f252fa8e4a97d25a52505e68f274924887415ad3
Cosmic <akshay@cosmicad.com>
2018-07-21 10:26:21 +0530

    VGG 16_KERAS_TO_TENSORFLOWJS

---
+++ VGG16_Keras_To_TensorflowJS/localserver/server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+
+app.use(function(req, res, next){
+    console.log(`${new Date()} - ${req.method} reqest for ${req.url}`);
+    next();
+});
+
+app.use(express.static("../static"));
+
+app.listen(8080, function(){
+    console.log("Serving at 8080")
+});
